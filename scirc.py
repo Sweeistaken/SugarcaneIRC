31e46ebe4a5bc81e62874c3ea6f817c581ebe6b8
Swee <sweeistaken@gmail.com>
2024-07-31 13:29:23 -0700

    Update scirc.py

--- scirc.py
+++ scirc.py
@@ -8,3 +8,30 @@ print("""
                   |___/                                                                      
 
 """)
+from time import sleep
+import sys
+import curses
+import threading
+sleep(5)
+stdscr = curses.initscr()
+curses.noecho()
+if not curses.has_colors():
+    raise SystemError("Your terminal doesn't support color!")
+rows, cols = stdscr.getmaxyx()
+curses.start_color()
+curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+stdscr.bkgd(' ', curses.color_pair(1))
+stdscr.attron(curses.color_pair(1) | curses.A_BOLD | curses.A_REVERSE)
+stdscr.addstr("[File] [Edit] [Connect]" + " "*(int(cols)-29) + "[Help]\n")
+stdscr.attroff(curses.color_pair(1) | curses.A_BOLD | curses.A_REVERSE)
+stdscr.attron(curses.color_pair(1))
+stdscr.addstr("Test")
+stdscr.attroff(curses.color_pair(1))
+curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_GREEN)
+stdscr.attron(curses.color_pair(2))
+stdscr.addstr("ing")
+stdscr.attroff(curses.color_pair(2))
+stdscr.refresh()
+sleep(3)
+curses.endwin()
+print("Exited successfully")

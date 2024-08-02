2ca33ed2e878c26b04a189b050f5aab9c8d82848
Swee <sweeistaken@gmail.com>
2024-08-01 21:32:07 -0700

    Update scirc.py

--- scirc.py
+++ scirc.py
@@ -12,14 +12,6 @@ from time import sleep
 import sys
 import curses
 import threading
-COLOR_GREY = 8
-COLOR_BRIGHT_BLUE = 9
-COLOR_BRIGHT_GREEN = 10
-COLOR_BRIGHT_CYAN = 11
-COLOR_BRIGHT_RED = 12
-COLOR_BRIGHT_MAGENTA = 13
-COLOR_BRIGHT_YELLOW = 14
-COLOR_BRIGHT_WHITE = 15
 sleep(1)
 stdscr = curses.initscr()
 curses.noecho()
@@ -28,13 +20,13 @@ if not curses.has_colors():
 rows, cols = stdscr.getmaxyx()
 curses.start_color()
 curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
-curses.init_pair(3, curses.COLOR_WHITE, COLOR_BRIGHT_BLUE)
+curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 stdscr.bkgd(' ', curses.color_pair(3))
 stdscr.attron(curses.color_pair(1))
 stdscr.addstr("[File] [Edit] [Connect]" + " "*(int(cols)-29) + "[Help]")
 stdscr.attroff(curses.color_pair(1))
 stdscr.addstr("Test")
-curses.init_pair(2, curses.COLOR_BLACK, COLOR_BRIGHT_GREEN)
+curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_GREEN)
 stdscr.attron(curses.color_pair(2))
 stdscr.addstr("ing")
 stdscr.attroff(curses.color_pair(2))
